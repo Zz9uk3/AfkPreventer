d2392d18faa261008974367e9ad46d6f0dce2a45
DESKTOP-RV4IPUB\Ano <zzuk@hacker.tl>
2017-10-04 21:18:33 +0200

    Initial commit

---
+++ AntiAfk/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Magic;
+using System.Diagnostics;
+
+namespace AntiAfk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlackMagic wow = new BlackMagic();
+            Console.WriteLine("Let the window remain open as long as you dont want to be flagged as AFK");
+            while (true)
+            {
+                Process[] p = Process.GetProcessesByName("WoW");
+                foreach (Process x in p)
+                {
+                    wow.OpenProcessAndThread(x.Id);
+                    string version = wow.ReadASCIIString(0x00837C04, 6);
+                    if (version == "1.12.1")
+                    {
+                        wow.WriteInt(0x00CF0BC8, Environment.TickCount);
+                    }
+                }
+                System.Threading.Thread.Sleep(10000);
+            }
+        }
+    }
+}
